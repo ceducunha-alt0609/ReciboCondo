816e290ea5d5821de711a8146e72a935857714c3
ReciboCondo Restore Guard <actions@users.noreply.github.com>
2026-08-27 16:01:30 +0000

    Fix DataCloneError when saving and restoring settings

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'recibocondo-';
-const CACHE_NAME = 'recibocondo-v74-installment-description';
+const CACHE_NAME = 'recibocondo-v75-restore-dataclone';
 const APP_SCOPE = '/ReciboCondo/';
 const APP_SHELL = [
   './',
