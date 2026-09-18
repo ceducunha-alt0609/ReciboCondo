55fb9df185a60979fdc9640d77d46b4d8eda2921
AquaControl <c.edu.cunha@gmail.com>
2026-09-18 17:04:50 -0300

    chore: atualiza cache para botao sync na topbar

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'recibocondo-';
-const CACHE_NAME = 'recibocondo-v159-multidesktop-sync';
+const CACHE_NAME = 'recibocondo-v160-topbar-sync';
 const APP_SCOPE = '/ReciboCondo/';
 const APP_SHELL = [
   './',
